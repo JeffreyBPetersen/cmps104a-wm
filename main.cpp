a5c52cb48648084a1af1e7aa58cdd4b000a7300d
JeffreyBPetersen <jebpeter@unix1.lt.ucsc.edu>
2014-10-21 04:37:12 -0700

    up-Added notes and corrected style

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ using namespace std;
 int main (int argc, char** argv) {
    set_execname (argv[0]);
    /*
+   // main from cppstrtok.cpp
+   
    for (int argi = 1; argi < argc; ++argi) {
       char* filename = argv[argi];
       string command = CPP + " " + filename;
@@ -28,12 +30,14 @@ int main (int argc, char** argv) {
    int option;
    while((option = getopt(argc, argv, "ly@:D:")) != -1){
       switch(option){
-			case '@':
-				fprintf(stderr, "Option '-@' not yet implemented, flags: %s\n", optarg);
-				break;
-			case 'D':
-				fprintf(stderr, "Option '-D' not yet implemented, string: %s\n", optarg);
-				break;
+         case '@':
+            fprintf(stderr, 
+            "Option '-@' not yet implemented, flags: %s\n", optarg);
+            break;
+         case 'D':
+            fprintf(stderr, 
+            "Option '-D' not yet implemented, string: %s\n", optarg);
+            break;
          case 'l':
             fprintf(stderr, "Option '-l' not yet implemented\n");
             break;
@@ -44,22 +48,27 @@ int main (int argc, char** argv) {
             set_exitstatus(EXIT_FAILURE);
       }
    }
-	if(optind < argc){
-		fprintf(stderr, "File input not yet implemented, file: %s\n", argv[optind]);
-		/*
-			ADD: Support directories, find file and separate out filename if necessary
-		*/
-	}
-	/*
-		ADD: Filter input through the C preprocessor, /usr/bin/cpp
-	*/
-	/*
-		ADD: Read line using fgets(3), 
-		tokenize it along whitespace using strtok_r(3) with the string " \t\n",
-		then insert it into the string set
+   if(optind < argc){
+      fprintf(stderr, 
+      "File input not yet implemented, file: %s\n", argv[optind]);
+      /*
+         ADD: Support directories, 
+         find file and separate out filename if necessary
+      */
+   }
+   /*
+      ADD: Filter input through the C preprocessor, /usr/bin/cpp
+      cppstrtok.cpp provides example,..
+      needs modification for returning tokens instead of printing them
    */
-	/*
-		ADD: Dump string set into trace file
-	*/
+   /*
+      ADD: Read line using fgets(3), //handled by cppstrtok
+      tokenize it along whitespace using strtok_r(3)..
+      with the string " \t\n", //handled by cppstrtok
+      then insert it into the string set
+   */
+   /*
+      ADD: Dump string set into trace file
+   */
    return get_exitstatus();
 }
