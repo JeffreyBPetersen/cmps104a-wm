3880edbcb8d97ad3d8af6049e4e9268696855924
JeffreyBPetersen <jebpeter@unix1.lt.ucsc.edu>
2014-10-21 07:05:27 -0700

    Removed commented out main from cppstrtok

--- main.cpp
+++ main.cpp
@@ -10,25 +10,6 @@ using namespace std;
 #include "cppstrtok.h"
 
 int main (int argc, char** argv) {
-   set_execname (argv[0]);
-   /*
-   // main from cppstrtok.cpp
-   
-   for (int argi = 1; argi < argc; ++argi) {
-      char* filename = argv[argi];
-      string command = CPP + " " + filename;
-      printf ("command=\"%s\"\n", command.c_str());
-      FILE* pipe = popen (command.c_str(), "r");
-      if (pipe == NULL) {
-         syserrprintf (command.c_str());
-      }else {
-         cpplines (pipe, filename);
-         int pclose_rc = pclose (pipe);
-         eprint_status (command.c_str(), pclose_rc);
-      }
-   }
-   */
-   
    int option;
    string path;
    string program_name;
