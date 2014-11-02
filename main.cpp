c83eab7048518d1d9a65ceefee614f2e36f932c6
JeffreyBPetersen <jeffreybpetersen@gmail.com>
2014-11-02 12:32:22 -0800

    corrected styling

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int main (int argc, char** argv) {
    int option;
    string path;
    string program_name;
-	
-	// get flag options
+   
+   // get flag options
    while((option = getopt(argc, argv, "ly@:D:")) != -1){
       switch(option){
          case '@':
@@ -38,35 +38,35 @@ int main (int argc, char** argv) {
    }
    for(int arg_index = optind; arg_index < argc; arg_index++){
       vector<string> tokens;
-		
-		// get program name
+      
+      // get program name
       path = argv[arg_index];
       program_name = basename(strdup(path.c_str()));
-		//ADD: check for .oc extension
+      //ADD: check for .oc extension
       program_name = program_name.substr(0, program_name.find("."));
       string command = CPP + " " + path;
       
-		// get piped input from c preprocessor
-		FILE* pipe = popen(command.c_str(), "r");
+      // get piped input from c preprocessor
+      FILE* pipe = popen(command.c_str(), "r");
       if(pipe == NULL){
          fprintf(stderr, "Failed to open file input pipe\n");
          set_exitstatus(EXIT_FAILURE);
       }else{
-		
-			// tokenize input from pipe
+      
+         // tokenize input from pipe
          tokens = cpplines(pipe, strdup(path.c_str()));
          int cpp_exit = pclose(pipe);
          if(cpp_exit != 0)
             set_exitstatus(1);
       }
-		
-		// add tokens to stringset
+      
+      // add tokens to stringset
       for(auto token_iter = tokens.begin();
       token_iter != tokens.end(); ++token_iter){
          intern_stringset(token_iter->c_str());
       }
-		
-		// output stringset to .str file
+      
+      // output stringset to .str file
       ofstream str_output;
       str_output.open(program_name + ".str");
       dump_stringset(str_output);
