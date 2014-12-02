bf3bcb5289355b83c4b1d127215205e16ba8fc2a
JeffreyBPetersen <jeffreybpetersen@gmail.com>
2014-12-02 06:24:40 -0800

    Begin symbol_table population implementation

--- symtable.cpp
+++ symtable.cpp
@@ -1,11 +1,26 @@
 // Jeffrey Petersen | jebpeter@ucsc.edu
 
 #include <fstream>
+#include <string>
 
+#include "astree.h"
 #include "symtable.h"
 
 ofstream sym_output;
+vector<symbol_table*> symbol_stack;
+symbol_table type_name_table;
+int next_block;
 
-void gen_symtable(struct astree* root){
+string reserved_words[] = {"void", "bool", "char", "int", "string"};
+
+void gen_symtable_rec(astree* node){
 	sym_output << "PLACEHOLDER\n";
+}
+
+void gen_symtable(astree* root){
+	for(int i = 0; i < 5; i++)
+		type_name_table.insert({&reserved_words[i],nullptr});
+	for(size_t child = 0; child < root->children.size(); ++child){
+      gen_symtable_rec(root->children[child]);
+   }
 }
